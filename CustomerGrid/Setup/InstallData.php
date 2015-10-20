5de6c71682b233662b0e3c5e892f7bfd5324656a
maksek <maxyek@gmail.com>
2015-10-20 16:27:18 -0500

    Add CustomerGrid module

---
+++ CustomerGrid/Setup/InstallData.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Maxyek\CustomerGrid\Setup;
+
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetup;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+
+    /**
+     * Customer setup factory
+     *
+     * @var \Magento\Customer\Setup\CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * Init
+     *
+     * @param \Magento\Customer\Setup\CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(\Magento\Customer\Setup\CustomerSetupFactory $customerSetupFactory)
+    {
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+
+    /**
+     * Installs DB schema for a module
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+        $customerSetup->addAttribute(
+            Customer::ENTITY,
+            'client_idn',
+            [
+
+                'label' => 'Client IDN',
+                'input' => 'text',
+                'required' => false,
+                'sort_order' => 40,
+                'visible' => true,
+                'system' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => true,
+                'is_filterable_in_grid' => true,
+                'is_searchable_in_grid' => true,
+                'used_in_forms', ['adminhtml_customer']
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}
---
+++ CustomerGrid/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Maxyek_CustomerGrid',
+    __DIR__
+);
